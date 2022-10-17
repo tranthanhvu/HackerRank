23b0a2ec3912c9b3971255ab0d15a9e7a542d021
Trần Thanh Vũ <ttvu90@gmail.com>
2022-10-18 02:44:59 +0700

    Update: Find Merge point of two lists

--- Practice/Data Structures/Linked Lists.playground/Pages/Find merge point of two lists.xcplaygroundpage/Contents.swift
+++ Practice/Data Structures/Linked Lists.playground/Pages/Find merge point of two lists.xcplaygroundpage/Contents.swift
@@ -33,25 +33,44 @@ final class SinglyLinkedList {
 
 /// Ref: https://www.hackerrank.com/challenges/find-the-merge-point-of-two-joined-linked-lists/problem
 func findMergeNode(head1: SinglyLinkedListNode?, head2: SinglyLinkedListNode?) -> SinglyLinkedListNode? {
-    var end1 = head1
-    var end2 = head2
+    var node1 = head1
+    var node2 = head2
     
-    var isFirstList = true
-    var currentNode = end1
-    
-    while end1 != nil && end2 != nil {
-        while currentNode != nil {
-            if currentNode === node2 {
-                return currentNode
+    while node1 != nil || node2 != nil {
+        if node1 != nil {
+            let find1 = findMergeNode(head: head2, end: node2, checkingNode: node1!)
+            if find1 != nil {
+                return find1
             }
-            
-            currentNode = currentNode?.next;
         }
         
+        if node2 != nil {
+            let find2 = findMergeNode(head: head1, end: node1, checkingNode: node2!)
+            if find2 != nil {
+                return find2
+            }
+        }
         
-        
+        node1 = node1?.next
+        node2 = node2?.next
     }
     
+    return nil
+}
+
+func findMergeNode(head: SinglyLinkedListNode?, end: SinglyLinkedListNode?, checkingNode: SinglyLinkedListNode) -> SinglyLinkedListNode? {
+    var currentNode = head
+    while currentNode != nil {
+        if currentNode === checkingNode {
+            return checkingNode
+        }
+        
+        if currentNode === end {
+            return nil
+        }
+        
+        currentNode = currentNode?.next
+    }
     
     return nil
 }
@@ -62,30 +81,52 @@ class UserManagerTests: XCTestCase {
     }
     
     func testCaseBase(input: [Int], expected: [Int?]) {
-        let testCaseCount = input.first!
-        
-        var currentIndex = 1
+        var currentIndex = 0
         func readInt() -> Int {
             let result = input[currentIndex]
             currentIndex += 1
             return result
         }
         
-        func readList() -> SinglyLinkedListNode? {
-            let count = readInt()
-            let linkedList = SinglyLinkedList()
-            for _ in 1...count {
-                let num = readInt()
-                linkedList.insertNode(nodeData: num)
-            }
-            
-            return linkedList.head
-        }
+        let testCaseCount = readInt()
         
         for testIndex in 1...testCaseCount {
-            let head1 = readList()
-            let head2 = readList()
+            let index = readInt()
             
+            let count1 = readInt()
+            let linkedList1 = SinglyLinkedList()
+            for _ in 1...count1 {
+                let num = readInt()
+                linkedList1.insertNode(nodeData: num)
+            }
+            
+            let head1 = linkedList1.head
+            
+            let count2 = readInt()
+            let linkedList2 = SinglyLinkedList()
+            for _ in 1...count2 {
+                let num = readInt()
+                linkedList2.insertNode(nodeData: num)
+            }
+            
+            let head2 = linkedList2.head
+            
+            var ptr1 = head1
+            var ptr2 = head2
+            
+            for i in 0..<count1 {
+                if i < index {
+                    ptr1 = ptr1?.next
+                }
+            }
+            
+            for i in 0..<count2 {
+                if i != count2 - 1 {
+                    ptr2 = ptr2?.next
+                }
+            }
+            
+            ptr2?.next = ptr1
             
             let result = findMergeNode(head1: head1, head2: head2)
             
@@ -93,10 +134,9 @@ class UserManagerTests: XCTestCase {
         }
     }
     
-    
-    
-    func testCase0() {
+    func testCasePre0() {
         testCaseBase(input: [
+            1,
             1,
             3,
             1, 2, 3,
@@ -105,9 +145,10 @@ class UserManagerTests: XCTestCase {
         ], expected: [2])
     }
     
-    func testCase1() {
+    func testCasePre1() {
         testCaseBase(input: [
             1,
+            2,
             3,
             1, 2, 3,
             2,
@@ -115,43 +156,27 @@ class UserManagerTests: XCTestCase {
         ], expected: [3])
     }
     
-//    func testCase2() {
-//        testCaseBase(input: [
-//            5,
-//            -1,
-//            21,
-//            5, 6, 8, 1, 6, 8, 2, 5, 5, 7,
-//            5, 7, 1, 4, 1, 5, 10, 1, 2, 2, 5,
-//            -1,
-//            85,
-//            9, 9, 9, 5, 1, 3, 10, 7, 8, 9,
-//            8, 5, 7, 9, 1, 3, 6, 5, 9, 6,
-//            10, 1, 10, 9, 4, 4, 1, 8, 8, 8,
-//            6, 7, 7, 4, 1, 7, 8, 3, 6, 8,
-//            3, 3, 2, 1, 1, 3, 3, 8, 9, 3,
-//            3, 9, 6, 5, 9, 9, 8, 1, 6, 7,
-//            9, 3, 5, 7, 6, 6, 5, 6, 10, 10,
-//            3, 2, 2, 6, 5, 5, 10, 7, 2, 1,
-//            2, 7, 1, 7, 3,
-//            -1,
-//            59,
-//            10, 10, 8, 10, 5, 3, 6, 2, 10, 10,
-//            7, 9, 10, 1, 2, 3, 9, 6, 7, 8,
-//            5, 1, 8, 6, 9, 10, 4, 1, 9, 8,
-//            2, 10, 9, 9, 9, 3, 1, 4, 5, 10,
-//            3, 3, 10, 4, 4, 4, 7, 2, 1, 5,
-//            1, 5, 5, 1, 2, 3, 10, 7, 5,
-//            -1,
-//            20,
-//            9, 3, 5, 9, 7, 7, 2, 3, 9, 7,
-//            6, 8, 2, 1, 3, 8, 2, 4, 5, 4,
-//            -1,
-//            30,
-//            4, 4, 6, 10, 8, 7, 4, 6, 6, 8,
-//            10, 4, 6, 8, 8, 9, 6, 4, 4, 6,
-//            7, 4, 8, 5, 7, 1, 1, 2, 1, 9,
-//        ], expected: [0, 0, 0, 0, 0])
-//    }
+    func testCase0() {
+        testCaseBase(input: [
+            1,
+            1,
+            3,
+            1, 2, 3,
+            1,
+            1
+        ], expected: [2])
+    }
+    
+    func testCase1() {
+        testCaseBase(input: [
+            1,
+            2,
+            3,
+            1, 2, 3,
+            1,
+            1,
+        ], expected: [3])
+    }
 }
 
 UserManagerTests.defaultTestSuite.run()
